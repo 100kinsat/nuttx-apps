ca81602adcd6ae851994d0138909bba73e1e0337
Reimanbow <reimanbow0318@gmail.com>
2026-05-18 17:17:38 +0900

    add: GPIOをトグルさせる

---
+++ 100kinsat/gpio_toggle/gpio_toggle_main.c
@@ -0,0 +1,17 @@
+#include <nuttx/config.h>
+#include <arch/board/cxd56_gpioif.h>
+#include <stdbool.h>
+
+#define GPIO_PIN    PIN_UART_TX
+
+int main(int argc, FAR char *argv[])
+{
+    board_gpio_config(GPIO_PIN, 0, false, true, PIN_FLOAT);
+    board_gpio_write(GPIO_PIN, 0);
+    bool ptn = false;
+
+    while (1) {
+        board_gpio_write(GPIO_PIN, ptn);
+        ptn = !ptn;
+    }
+}
